2759c0f1813eba9476136c62ec2484a42bdb92aa
rkaranam <karanam.rajasekhar44@gmail.com>
2016-12-20 13:41:28 +0530

    Moved methods to RestController

--- src/main/java/org/learnspring/samples/controller/MainController.java
+++ src/main/java/org/learnspring/samples/controller/MainController.java
@@ -1,30 +1,16 @@
 package org.learnspring.samples.controller;
 
-import java.util.List;
-
-import org.learnspring.samples.model.Task;
-import org.learnspring.samples.service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+/* Simple Spring MVC Controller for JSPs */
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	private TaskService taskService;
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "index";
-	}
-	
-	@RequestMapping("/all-tasks")
-	public @ResponseBody String allTasks() {
-		List<Task> tasks = taskService.findAllTasks();
-		return tasks.toString();
 	}
 
 }
--- src/main/java/org/learnspring/samples/controller/SampleRestController.java
+++ src/main/java/org/learnspring/samples/controller/SampleRestController.java
@@ -1,14 +1,28 @@
 package org.learnspring.samples.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.learnspring.samples.model.Task;
+import org.learnspring.samples.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleRestController {
 	
-	@RequestMapping("hello")
+	@Autowired
+	private TaskService taskService;
+	
+	@RequestMapping("/home")
 	public String printHello() {
-		return "SampleRestController: printHello()";
+		return "Welcome to Task Manager!";
+	}
+	
+	@RequestMapping("/all-tasks")
+	public String allTasks() {
+		List<Task> tasks = taskService.findAllTasks();
+		return tasks.toString();
 	}
 
 }
